52d2472d3f035aeaf16620da504fcdf073f25564
Tomas King <tomas.king@asos.com>
2015-12-17 09:11:18 +0000

    Generating Random Numbers with crypto

---
+++ SecurityDemo/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace SecurityDemo
+{
+    using System;
+    using System.Security.Cryptography;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(Convert.ToBase64String(GenerateRandomNumber(32)));
+            }
+            Console.ReadLine();
+        }
+
+        public static byte[] GenerateRandomNumber(int length)
+        {
+            using (var randomGenerator = new RNGCryptoServiceProvider())
+            {
+                var randomNumber = new byte[length];
+                randomGenerator.GetBytes(randomNumber);
+                return randomNumber;
+            }
+        }
+    }
+}
